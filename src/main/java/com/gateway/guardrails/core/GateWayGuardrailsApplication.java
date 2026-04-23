43b52027ab9f5dc5aa8661590f0e5a2bb384afb8
UniStar903 <mygithub.903@gmail.com>
2026-04-24 02:03:23 +0530

    rebased the remote main and local main

---
+++ src/main/java/com/gateway/guardrails/core/GateWayGuardrailsApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.guardrails.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayGuardrailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayGuardrailsApplication.class, args);
+	}
+
+}
