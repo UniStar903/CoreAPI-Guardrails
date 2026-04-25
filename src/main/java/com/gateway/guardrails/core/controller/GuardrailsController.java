be447cba3d8611b5433a432b4b413b5fef2f0da6
UniStar903 <mygithub.903@gmail.com>
2026-04-26 01:30:56 +0530

    likedAPost added

--- src/main/java/com/gateway/guardrails/core/controller/GuardrailsController.java
+++ src/main/java/com/gateway/guardrails/core/controller/GuardrailsController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.gateway.guardrails.core.dto.CreateCommentRequest;
 import com.gateway.guardrails.core.entity.Comment;
+import com.gateway.guardrails.core.repository.GuardrailsPostRepo;
+import com.gateway.guardrails.core.service.GuardrailViralityService;
 import com.gateway.guardrails.core.service.GuardrailsCommentService;
+import com.gateway.guardrails.core.service.GuardrailsPostService;
 
 @RestController
 @RequestMapping("/api")
@@ -22,6 +26,10 @@ public class GuardrailsController {
 	
 	@Autowired
 	private GuardrailsCommentService commentService;
+	@Autowired
+	private GuardrailsPostService postService;
+	@Autowired
+    private GuardrailViralityService viralityService;
 	
 	@PostMapping("/posts")
 	public String createNewPost() {
@@ -50,8 +58,16 @@ public class GuardrailsController {
         }
     }
 	
-	@PostMapping("/posts/{postId}/like")
-	public ResponseEntity<List<Comment>> likedAPost(@PathVariable(required = true) long postId) {
-        return ResponseEntity.ok(commentService.getCommentsByPostId(postId));
+	@GetMapping("/posts/{postId}/comments")
+	public ResponseEntity<List<Comment>> getComments(@PathVariable(required = true) long postId){
+		return ResponseEntity.ok(commentService.getCommentsByPostId(postId));
 	}
+	
+	@PostMapping("/posts/{postId}/like")
+	public ResponseEntity<Map<String, String>> likedAPost(@PathVariable(required = true) long postId,@RequestBody Map<String, Object> request) {        
+//		String authorType = (String) request.get("authorType");
+//		Long authorId = ((Number) request.get("authorId")).longValue();
+		viralityService.updateViralityScore(postId, "HUMAN_LIKE");
+		return ResponseEntity.ok(Map.of("message", "Post liked successfully"));
+    }
 }
